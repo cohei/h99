9e5d42397aea973070d92876fe15af5e07d71d0d
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2017-10-12 09:19:41 +0900

    Add h40

--- H99.hs
+++ H99.hs
@@ -517,3 +517,20 @@ h38 = h34 10090 == h37 10090
 h39 :: Int -> Int -> [Int]
 -- h39 = _
 h39 n m = filter h31 [n..m]
+
+-- | (**) Goldbach's conjecture.
+--
+-- Goldbach's conjecture says that every positive even number greater than 2 is the sum of two prime numbers. Example: 28 = 5 + 23. It is one of the most famous facts in number theory that has not been proved to be correct in the general case. It has been numerically confirmed up to very large numbers (much larger than we can go with our Prolog system). Write a predicate to find the two prime numbers that sum up to a given even integer.
+--
+-- Example:
+--
+-- >>> h40 28
+-- (5,23)
+h40 :: Int -> (Int, Int)
+h40 n
+  | odd n = error "must be even"
+  | otherwise =
+    let
+      ps = h39 2 (n - 2)
+    in
+      head [ (p1, p2) | p1 <- ps, p2 <- ps, p1 <= p2, p1 + p2 == n ]
